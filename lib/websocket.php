d8c6e773e51203960d5874a46b4dcfbeb5df6f44
楊斯惟 <yangszwei@outlook.com>
2023-05-30 10:42:33 +0800

    Update websocket.php

--- lib/websocket.php
+++ lib/websocket.php
@@ -2,7 +2,23 @@
 
     class WebSocket
     {
+        /**
+         * The on message callback.
+         *
+         * Usage:
+         *
+         * ```
+         * $server->on_message = function (Socket $socket, string $message) {
+         *    // Do something with the message.
+         * };
+         * ```
+         */
+        public Closure $on_message;
+
+        /** The server socket. */
         private Socket $server;
+
+        /** The connected clients, indexed by their id. */
         private array $clients = [];
 
         function __construct(string $address, int $port)
@@ -10,11 +26,16 @@
             $this->server = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
             socket_set_option($this->server, SOL_SOCKET, SO_REUSEADDR, 1);
             socket_bind($this->server, $address, $port);
-            socket_listen($this->server);
+            $this->on_message = fn(Socket $socket, string $message) => null;
         }
 
-        function run(): void
+        /** Starts listening for connections. */
+        function listen(): void
         {
+            socket_listen($this->server);
+
+            file_put_contents("php://stdout", "Server started listening!\n");
+
             while (true) {
                 $changed = array_merge([$this->server], $this->clients);
                 $write = $except = null;
@@ -22,37 +43,72 @@
 
                 if (in_array($this->server, $changed)) {
                     $client = socket_accept($this->server);
-                    $this->clients[] = $client;
+                    $id = uniqid();
+                    $this->clients[$id] = $client;
 
                     self::perform_handshake($client);
+
+                    $this->send($client, json_encode([
+                        "type" => "init",
+                        "id" => $id
+                    ]));
 
                     $index = array_search($this->server, $changed);
                     unset($changed[$index]);
                 }
 
                 foreach ($changed as $socket) {
-                    while (socket_recv($socket, $data, 1024, 0) >= 1) {
-                        $message = self::decode($data);
-                        self::broadcast(self::encode($message));
-                        break 2;
-                    }
+                    if ($socket !== $this->server) {
+                        while (socket_recv($socket, $data, 1024, 0) >= 1) {
+                            $message = self::decode_message($data);
+                            ($this->on_message)($socket, $message);
+                            break 2;
+                        }
 
-                    $bytes = @socket_read($socket, 1024, PHP_NORMAL_READ);
-                    if ($bytes === false) {
-                        $index = array_search($socket, $this->clients);
-                        unset($this->clients[$index]);
+                        $bytes = @socket_read($socket, 1024, PHP_NORMAL_READ);
+                        if ($bytes === false) {
+                            $index = array_search($socket, $this->clients);
+                            unset($this->clients[$index]);
+                        }
                     }
                 }
             }
         }
 
+        /**
+         * Sends a message to a client.
+         *
+         * @param Socket | string $client The client to send the message to or its id.
+         * @param string $message The message to send.
+         */
+        function send(Socket|string $client, string $message): void
+        {
+            if (is_string($client)) $client = $this->clients[$client];
+            $encoded_message = self::encode_message($message);
+            socket_write($client, $encoded_message, strlen($encoded_message));
+        }
+
+        /**
+         * Broadcasts a message to all connected clients.
+         *
+         * @param string $message The message to broadcast.
+         * @return void
+         */
         function broadcast(string $message): void
         {
+            $encoded_message = self::encode_message($message);
+            $length = strlen($encoded_message);
             foreach ($this->clients as $client) {
-                socket_write($client, $message, strlen($message));
+                socket_write($client, $encoded_message, $length);
             }
         }
 
+        /**
+         * Performs the opening handshake with a client.
+         *
+         * @param Socket $client The client to perform the handshake with.
+         * @return void
+         */
         private static function perform_handshake(Socket $client): void
         {
             $headers = array();
@@ -77,7 +133,13 @@
             socket_write($client, $upgrade, strlen($upgrade));
         }
 
-        private static function encode(string $message): string
+        /**
+         * Encodes a message to be sent to a client.
+         *
+         * @param string $message The message to encode.
+         * @return string The encoded message.
+         */
+        private static function encode_message(string $message): string
         {
             $length = strlen($message);
 
@@ -92,7 +154,13 @@
             return $header . $message;
         }
 
-        private static function decode(string $message): string
+        /**
+         * Decodes a message from a client.
+         *
+         * @param string $message The message to decode.
+         * @return string The decoded message.
+         */
+        private static function decode_message(string $message): string
         {
             $length = ord($message[1]) & 127;
 
